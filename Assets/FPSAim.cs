29434153a5c235a89c49f9e7fcc7e5f68e17fc68
fearARGENTINA <bravogermanezequiel@gmail.com>
2018-12-19 22:17:42 -0300

    Primer Push
    Git GUI y Unity 3D

---
+++ Assets/FPSAim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSAim : MonoBehaviour {
+
+    public float mouseSensitivity = 1.0f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        yaw += mouseSensitivity * Input.GetAxis("Mouse X");
+        pitch -= mouseSensitivity * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0);
+	}
+}
---
+++ Assets/FPSWalk.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSWalk : MonoBehaviour
+{
+
+    private float playerVelocity = 0.1f;
+    private float playerJumpforce = 20.0f;
+    private Rigidbody mRigidbody = null;
+    private Collider mCollider = null;
+    private float distToGround;
+    // Use this for initialization
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+        mCollider = GetComponent<Collider>();
+
+        distToGround = mCollider.bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal") * playerVelocity;
+        float zAxisValue = Input.GetAxis("Vertical") * playerVelocity;
+
+        transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue));
+
+        if (mRigidbody != null && mCollider != null && IsGrounded() && Input.GetKey(KeyCode.Space))
+        {
+            mRigidbody.AddForce(Vector3.up * playerJumpforce);
+        }
+    }
+
+    private bool IsGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+}
